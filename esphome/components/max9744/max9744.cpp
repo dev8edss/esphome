57f71dbe897dcfab2d6035ec6a68e43032fcb94b
dev8edss <77666361+dev8edss@users.noreply.github.com>
2024-01-30 17:46:26 +0800

    Update max9744.cpp

--- esphome/components/max9744/max9744.cpp
+++ esphome/components/max9744/max9744.cpp
@@ -13,7 +13,7 @@ void MAX9744::dump_config() {
 }
 
 void MAX9744::write_state(float state) {
-  const uint8_t value = remap(state, 0.0f, 1.0f, 0, 63);
+  const uint8_t value = remap(state, 0.0f, 1.0f, 63, 0);
   this->write_byte(0, value);
   ESP_LOGCONFIG(TAG, "MAX9744 Output:");
   LOG_I2C_DEVICE(this);
